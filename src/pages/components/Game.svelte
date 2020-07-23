ccd26f453a79d866fd1a95dd4dbdaffa78c81f4e
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2020-07-23 16:44:41 +0000

    update design & remove bugs :bug:

--- src/pages/components/Game.svelte
+++ src/pages/components/Game.svelte
@@ -3,6 +3,7 @@
   let data = {};
   let data_base = {};
   let current_question = {};
+  let conf_reset_display = false;
   const available_catagories = [
     "food",
     "guys",
@@ -22,13 +23,22 @@
     current_catagory: []
   };
 
-  fetch("https://api.npoint.io/7b9ef3a72b45bd130e80")
-    .then(response => response.json())
-    .then(json => {
-      data_base = json;
-      data = json;
-    })
-    .then($ready);
+  init();
+
+  async function init() {
+    const res = await fetch("https://api.npoint.io/7b9ef3a72b45bd130e80");
+    let json_res = JSON.stringify(await res.json());
+    data_base = JSON.parse(json_res);
+    data = JSON.parse(json_res);
+    $ready();
+  }
+
+  function conf_reset() {
+    conf_reset_display = true;
+    setTimeout(() => {
+      conf_reset_display = false;
+    }, 2500);
+  }
 
   function toggleSelection(catagory) {
     let newState = game_state;
@@ -67,17 +77,33 @@
 
     const sel_question = chooseQuestionFromCatagory(sel_cat);
 
-    if (sel_question === undefined) {
+    console.log(sel_question, game_state.current_catagory.length);
+    if (
+      sel_question === undefined &&
+      game_state.current_catagory.length === 1
+    ) {
       let newState = game_state;
       newState.game_completed = true;
       game_state = { ...newState };
+    } else if (
+      sel_question === undefined &&
+      game_state.current_catagory.length >= 1
+    ) {
+      let newStateX = game_state;
+      newStateX.current_catagory.splice(
+        newStateX.current_catagory.indexOf(sel_cat),
+        1
+      );
+      game_state = { ...newStateX };
+      current_question = selectQuestion();
+      return;
     }
 
     console.log(`Catagory: ${sel_cat}; question: ${sel_question}`);
-    current_question = {
+    return (current_question = {
       question_content: sel_question,
       question_catagory: sel_cat
-    };
+    });
   }
   function chooseQuestionFromCatagory(cat) {
     const rand_number = getRandomInt(0, data[cat].length);
@@ -94,19 +120,17 @@
     game_state = { ...newState };
     //reset to api data
     data = data_base;
+    // set conf display to false
+    conf_reset_display = false;
   }
 </script>
 
 <style>
-  .container {
-    position: relative;
-    max-width: 600px;
-    margin: auto;
-  }
   button {
-    border-radius: 8px;
-    padding: 6px 10px;
-    border: 2px solid #000;
+    margin: 12px 6px;
+    width: auto;
+    padding-right: 8px;
+    padding-left: 8px;
   }
   .catagories {
     width: 100%;
@@ -116,32 +140,18 @@
     flex-wrap: wrap;
   }
   .catagory {
-    width: 200px;
+    max-width: 200px;
     margin: 12px auto;
-    border-radius: 8px;
-    box-shadow: 3px 3px 12px #d9d9d9, -3px -3px 12px #ffffff;
     padding: 2px 10px;
     text-align: center;
   }
-  .catagory.active {
-    box-shadow: inset 3px 3px 12px #d9d9d9, inset -3px -3px 12px #ffffff;
-    background-color: #f8f8f8;
-  }
-  .catagory p {
-    text-transform: capitalize;
-    user-select: none;
-  }
-  .nextln {
-    display: block;
-    float: left;
+  .checktext {
+    margin: 10px 0;
   }
   .question_container {
-    margin: 10px 0;
+    margin: 10px auto;
     max-width: 500px;
     padding: 12px;
-    background-color: #f4f4f4;
-    border-radius: 8px;
-    box-shadow: inset 0 0 4px 0 rgba(0, 0, 0, 0.1);
   }
   .question_container .small {
     margin: 0;
@@ -153,54 +163,66 @@
     font-size: 18px;
     margin: 4px 0;
   }
-  button.primary {
-    background-color: rgb(40, 145, 206);
-    color: #fff;
+  .paper {
+    margin: 24px auto;
+  }
+  p.nomore {
+    text-align: center;
+    color: #afafaf;
+    font-size: 15px;
+    letter-spacing: 0.5px;
+    font-weight: bolder;
   }
 </style>
 
-<div class="container">
-  <div class="board">
-    {#if !game_state.game_completed}
-      {#if game_state.catagory_select}
-        <div class="catagories">
-          {#each available_catagories as cat}
-            <div
-              class="catagory"
-              class:active={game_state.current_catagory.includes(cat)}
-              on:click={() => toggleSelection(cat)}>
-              <p>{cat}</p>
-            </div>
-          {/each}
-        </div>
-        <div class="nextln" />
-        <button class="primary" on:click={() => confirmSelections()}>
-          Continue
-        </button>
-      {:else}
-        <h1>Game Board</h1>
-        <div class="nextln" />
-        {#if current_question.question_content !== undefined}
-          <div class="question_container">
-            <p class="small">Catagory: {current_question.question_catagory}</p>
-            <p class="question">{current_question.question_content}</p>
+<div class="board">
+  {#if !game_state.game_completed}
+    {#if game_state.catagory_select}
+      <div class="catagories">
+        {#each available_catagories as cat}
+          <div class="catagory">
+            <label class="checktext">
+              <input
+                type="checkbox"
+                class="catagory"
+                bind:group={game_state.current_catagory}
+                value={cat}
+                checked={game_state.current_catagory.includes(cat)} />
+              <span>{cat}</span>
+            </label>
           </div>
-        {:else}
-          <h2>Choose a question</h2>
-        {/if}
-        <div class="nextln" />
-        <button on:click={() => selectCatagories()}>
-          Return to catagory selection
-        </button>
-        <button on:click={() => reset()}>Reset Game</button>
-
-        <button class="primary" on:click={() => selectQuestion()}>
-          Next Question
-        </button>
-      {/if}
+        {/each}
+      </div>
+      <button class="green-button" on:click={() => confirmSelections()}>
+        Continue
+      </button>
     {:else}
-      <h1>There are no more questions...</h1>
-      <button on:click={() => reset()}>Reset Game</button>
+      {#if current_question.question_content !== undefined}
+        <div class="paper question_container">
+          <p class="small">Catagory: {current_question.question_catagory}</p>
+          <p class="question">{current_question.question_content}</p>
+        </div>
+      {:else}
+        <h2>Choose a question</h2>
+      {/if}
+      <hr />
+      <button class="red-button" on:click={() => conf_reset()}>
+        Reset Game
+      </button>
+      <button style="width: auto;" on:click={() => selectCatagories()}>
+        Return to catagory selection
+      </button>
+
+      <button class="green-button" on:click={() => selectQuestion()}>
+        Next Question
+      </button>
     {/if}
-  </div>
+  {:else}
+    <p class="nomore">There are no more questions...</p>
+    <button on:click={() => reset()}>Reset Game</button>
+  {/if}
+  {#if conf_reset_display}
+    <hr />
+    <button class="red-button" on:click={() => reset()}>Confirm Reset</button>
+  {/if}
 </div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,9 +1,10 @@
 <script>
-  const name = "Never Have I Ever App";
+  const name = "Never have I ever";
   const version = "0.0.1";
   import NeverHaveIEver from "./components/Game.svelte";
   import { metatags } from "@sveltech/routify";
-  metatags.title, (metatags.description = name);
+  metatags.title = name;
+  metatags.description = name;
   metatags.version = version;
 </script>
 
@@ -11,8 +12,22 @@
   h1 {
     text-align: center;
   }
+  p.madeby {
+    color: #afafaf;
+    font-size: 15px;
+    letter-spacing: 0.5px;
+    font-weight: bolder;
+  }
+  .container {
+    position: relative;
+    max-width: 600px;
+    margin: auto;
+  }
 </style>
 
-<h1>{name}</h1>
-<NeverHaveIEver />
-<p>Made by Kieran</p>
+<div class="container">
+  <h1>{name}</h1>
+  <hr />
+  <NeverHaveIEver />
+  <p class="madeby">Made by Kieran</p>
+</div>
