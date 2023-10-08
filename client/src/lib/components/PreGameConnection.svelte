e71fe75abfd13d2516cd22a0b33c177399a2cda3
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2023-10-08 16:30:46 +0100

    change style

--- client/src/lib/components/PreGameConnection.svelte
+++ client/src/lib/components/PreGameConnection.svelte
@@ -5,16 +5,16 @@
 	export let players: Player[] = [];
 </script>
 
-<div class="text-center">
+<div class="text-center uppercase text-xs">
 	{#if connection === Status.CONNECTED}
-		<p class="text-green-700">
+		<p class="dark:text-green-400 text-green-700">
 			Connected <span class="dark:text-white text-black">
 				({players.filter((p) => p.connected).length})
 			</span>
 		</p>
 	{:else if connection === Status.DISCONNECTED}
-		<p class="text-red-700">Disconnected</p>
+		<p class="dark:text-red-400 text-red-700">Disconnected</p>
 	{:else}
-		<p class="text-red-700">Connecting...</p>
+		<p class="dark:text-gray-400 text-gray-700">Connecting...</p>
 	{/if}
 </div>
