d7e9f51760453035713777c72903650796026218
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2023-10-12 00:15:07 +0100

    move to head

--- client/src/routes/play/[gameid]/+page.svelte
+++ client/src/routes/play/[gameid]/+page.svelte
@@ -6,29 +6,31 @@
 	export let data: PageData;
 </script>
 
-<title>
-	{data.game?.active ? '[LIVE]' : ''} Play Never Have I Ever Online
-</title>
-{#if data.game?.active}
-	<meta
-		name="description"
-		content="Join {data.game?.players
-			?.filter((p) => p.connected)
-			.map((p) => p.name)
-			?.join(', ')} playing never have i ever online!"
-	/>
-	<meta
-		property="og:title"
-		content="{data.game?.active ? '[LIVE]' : ''} Play Never Have I Ever Online"
-	/>
-	<meta
-		property="og:description"
-		content="Join {data.game?.players
-			?.filter((p) => p.connected)
-			.map((p) => p.name)
-			?.join(', ')} playing never have i ever online!"
-	/>
-	<meta property="og:type" content="website" />
-	<meta property="og:site_name" content="Never Have I Ever ~ games.kieran.dev" />
-{/if}
+<svelte:head>
+	<title>
+		{data.game?.active ? '[LIVE]' : ''} Play Never Have I Ever Online
+	</title>
+	{#if data.game?.active}
+		<meta
+			name="description"
+			content="Join {data.game?.players
+				?.filter((p) => p.connected)
+				.map((p) => p.name)
+				?.join(', ')} playing never have i ever online!"
+		/>
+		<meta
+			property="og:title"
+			content="{data.game?.active ? '[LIVE]' : ''} Play Never Have I Ever Online"
+		/>
+		<meta
+			property="og:description"
+			content="Join {data.game?.players
+				?.filter((p) => p.connected)
+				.map((p) => p.name)
+				?.join(', ')} playing never have i ever online!"
+		/>
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content="Never Have I Ever ~ games.kieran.dev" />
+	{/if}
+</svelte:head>
 <NeverHaveIEver id={$page.params.gameid} />
