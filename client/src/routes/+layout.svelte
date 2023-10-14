946fe2e6615c85128bddf17f5aa3e7ef012dc280
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2023-10-15 00:02:46 +0100

    add seo keywords

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script>
 	import '../app.css';
+	import keywords from '$lib/assets/keywords.json';
 </script>
 
+<svelte:head>
+	<title>Games ~ Kieran.dev</title>
+	<meta
+		name="description"
+		content="A collection* of games to play with friends (*not actually a collection yet)"
+	/>
+	<meta name="keywords" content={keywords.join(', ')} />
+</svelte:head>
 <div class="max-w-4xl mx-auto">
 	<slot />
 </div>
