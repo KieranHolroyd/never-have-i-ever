775ee06c5aa1142a2ff28c760f48abec419294c0
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2023-10-14 19:28:07 +0100

    add back button, temp remove form

--- client/src/routes/suggest/+page.svelte
+++ client/src/routes/suggest/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import MdiEmailMultiple from 'virtual:icons/mdi/email';
+	import MdiEmailMultiple from '~icons/mdi/email';
+	import MdiKeyboardBackspace from '~icons/mdi/keyboard-backspace';
 </script>
 
-<div class="prose prose-invert lg:prose-lg xl:prose-xl">
+<div class="p-2 prose prose-invert lg:prose-lg xl:prose-xl">
 	<h1>Contact us</h1>
 	<p>
 		We are always looking for ways to improve our service. If you have any suggestions, bug reports,
@@ -13,8 +14,7 @@
 			<MdiEmailMultiple class="inline ml-2 mr-1" />games@kieran.dev
 		</a>
 	</p>
-	<form method="POST" action="?/suggest">
-		<h1>The form doesn't work yet!</h1>
+	<!-- <form method="POST" action="?/suggest">
 		<div class="grid grid-cols-1 gap-6">
 			<label class="block">
 				<span class="dark:text-gray-200 text-gray-700">Full name</span>
@@ -43,12 +43,19 @@
 					required
 				/>
 			</label>
-		</div>
+		</div> -->
+	<a href="/">
 		<button
+			class="relative pl-10 dark:text-white bg-gray-400 dark:bg-gray-600 hover:bg-gray-200 hover:dark:bg-gray-900 mt-2 px-4"
+		>
+			<MdiKeyboardBackspace class="absolute top-2.5 lg:top-3 xl:top-3.5 left-2 h-6 w-6" />Back
+		</button>
+	</a>
+	<!-- <button
 			type="submit"
 			class="dark:text-white bg-gray-400 dark:bg-gray-800 hover:bg-gray-200 hover:dark:bg-gray-900 mt-2 px-4"
 		>
 			Send
-		</button>
-	</form>
+		</button> -->
+	<!-- </form> -->
 </div>
