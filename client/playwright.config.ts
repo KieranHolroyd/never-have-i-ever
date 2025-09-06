b579c87491bc955fb682f4968bd70553032c49ca
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2025-09-07 00:47:15 +0100

    slow it a little

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Use multiple workers for speed */
-  workers: process.env.CI ? 12 : 4,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
