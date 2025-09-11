a74c3902c254d8c4de9893351ea8fc3b50a06cb1
Kieran Holroyd <kieran@nitrexdesign.co.uk>
2025-09-11 06:11:37 +0100

    change playwright config

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 1 : 4,
 	/* Use multiple workers for speed */
 	workers: 4,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
